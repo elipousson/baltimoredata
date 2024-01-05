8c63bfb0df1f8229ff5e7e9ab350c9169aae1b61
your name <eli.pousson@gmail.com>
2024-01-05 10:05:57 -0500

    feat: update entity_reference
    Also add script for backing up additional tables

--- data-raw/public_entities.R
+++ data-raw/public_entities.R
@@ -2,6 +2,54 @@
 # FIXME: Replace this version if Baltimore City Entity Reference (2023-11-06
 # Copy) is ever converted
 # https://airtable.com/appJ92OybyLWmOqLh/shrM4MB0YoQk7cusC
+entities_list <- rairtable::list_records(
+  url = "https://airtable.com/appJ92OybyLWmOqLh/tblgKp8tPh0BtgB1e/viwJsYeLTUq0dlTmg?blocks=hide",
+  cell_format = "json",
+  simplifyVector = FALSE
+)
+
+entities_list |>
+  jsonlite::write_json("inst/extdata/entities.json")
+
+people_list <- rairtable::list_records(
+  url = "https://airtable.com/appJ92OybyLWmOqLh/tblH1Nd6Y2kiCz9yq/viw5h2i5Y0W09kdJM?blocks=hide",
+  cell_format = "json",
+  simplifyVector = FALSE
+)
+
+people_list |>
+  jsonlite::write_json("inst/extdata/people.json")
+
+
+assignments_list <- rairtable::list_records(
+  url = "https://airtable.com/appJ92OybyLWmOqLh/tblnCgC6OokekMbO5/viwYUQp8C1nYFPRVG?blocks=hide",
+  cell_format = "json",
+  simplifyVector = FALSE
+)
+
+assignments_list |>
+  jsonlite::write_json("inst/extdata/assignments.json")
+
+
+sources_list <- rairtable::list_records(
+  url = "https://airtable.com/appJ92OybyLWmOqLh/tblt2VGrpCu2YW1sk/viwRHB1ZHcKM7VV2g?blocks=hide",
+  cell_format = "json",
+  simplifyVector = FALSE
+)
+
+sources_list |>
+  jsonlite::write_json("inst/extdata/sources.json")
+
+
+crosswalks_list <- rairtable::list_records(
+  url = "https://airtable.com/app1lcJCwi0mpQGqZ/tbl81zsVzjBxVZePB/viwlrhbxPrDasYqzp?blocks=hide",
+  cell_format = "json",
+  simplifyVector = FALSE
+)
+
+crosswalks_list |>
+  jsonlite::write_json("inst/extdata/crosswalks.json")
+
 entity_reference_source <- rairtable::list_records(
   url = "https://airtable.com/appJ92OybyLWmOqLh/tblgKp8tPh0BtgB1e/viwJsYeLTUq0dlTmg?blocks=hide",
   cell_format = "string"
